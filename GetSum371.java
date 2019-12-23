a04808a121feb82e45cc0f3e7f6b4e5486539ad9
lisiyu06 <18392609095@163.com>
2019-12-23 09:05:16 +0800

    两整数之和

--- GetSum371.java
+++ GetSum371.java
@@ -2,6 +2,10 @@
  * Author: lisiyu
  * Created: 2019/12/21
  */
+
+// LeetCode 371 ----- 两整数之和
+    // 不使用运算符 + 和 -，计算两整数 a 、b 之和。
+
 public class GetSum371 {
     public int getSum(int a, int b) {
         while(b != 0) {
